278f4daef7f04078f0f1da84d9a186de98d7d89c
João Prizal <joao.prizal@inventsys.ch>
2022-12-27 01:02:47 +0000

    Adicionar quiz

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,16 @@
 
-D
-assets/css/boxes.css,7/4/741c25589538691478bf45ec051f6dfd46a5922a
-K
-pages/fatores/fatoresF.html,b/5/b573dc922fb2903a2bbdf68f81c181a132d6adf2
-K
-pages/fatores/fatoresM.html,9/0/90d240c80dda94411ce764cb879bdc2b057b6b89
+I
+site/assets/css/modal.css,3/b/3b8f8556727591c04606617d5b830bf07d1c7f54
+I
+site/assets/css/login.css,d/0/d07fe596dcfae421288e38a9e4e42042bcbad9ab
+I
+site/assets/css/sound.css,8/1/814cfb1171a6235e248495d7ba201076ec05e38c
+L
+site/assets/scripts/sound.js,a/c/ac4156d1a3be82c37685a410ea6d44c8670424c7
+J
+site/assets/css/login.scss,c/0/c0cda6b79173a5d8489ac8feca0b6fdcd1991a57
+L
+site/assets/scripts/modal.js,4/0/40a767754d4df1b78938e65cfd8463215c005be8
+:
+
+index.html,f/6/f6013a00b362253c64368d6eebc50ea2131754e2
--- site/assets/scripts/quiz.js
+++ site/assets/scripts/quiz.js
@@ -1,6 +1,41 @@
-var quiz = [
-    {
-        pergunta: "Qual ",
-        resposta: ""
+var correcta1 = document.querySelector('#quizform input[name="resposta1"][value="1"]')
+var correcta2 = document.querySelector('#quizform input[name="resposta2"][value="1"]')
+var correcta3 = document.querySelector('#quizform input[name="resposta3"][value="4"]')
+var correcta4 = document.querySelector('#quizform input[name="resposta4"][value="3"]')
+var correcta5 = document.querySelector('#quizform input[name="resposta5"][value="2"]')
+var form = document.getElementById('quizform')
+
+form.onsubmit = function (event) {
+    event.preventDefault()
+    var inputs = form.getElementsByTagName('input')
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].closest('.form-control').style.display = 'none'
     }
-]
+    correcta1.closest('.form-control').style.display = 'block'
+    correcta2.closest('.form-control').style.display = 'block'
+    correcta3.closest('.form-control').style.display = 'block'
+    correcta4.closest('.form-control').style.display = 'block'
+    correcta5.closest('.form-control').style.display = 'block'
+
+    correcta1.closest('.form-control').style.backgroundColor = 'green'
+    correcta2.closest('.form-control').style.backgroundColor = 'green'
+    correcta3.closest('.form-control').style.backgroundColor = 'green'
+    correcta4.closest('.form-control').style.backgroundColor = 'green'
+    correcta5.closest('.form-control').style.backgroundColor = 'green'
+
+    document.querySelector('#quizform input[name="resposta1"]:checked').closest('.form-control').style.display = 'block'
+    document.querySelector('#quizform input[name="resposta2"]:checked').closest('.form-control').style.display = 'block'
+    document.querySelector('#quizform input[name="resposta3"]:checked').closest('.form-control').style.display = 'block'
+    document.querySelector('#quizform input[name="resposta4"]:checked').closest('.form-control').style.display = 'block'
+    document.querySelector('#quizform input[name="resposta5"]:checked').closest('.form-control').style.display = 'block'
+    document.querySelector('#quizform button[type="submit"]').style.display = 'none'
+    document.querySelector('#quizform #recomecarbtn').style.display = 'block'
+}
+
+/**
+ * https://www.w3schools.com/jsref/met_loc_reload.asp
+ */
+document.querySelector('#recomecarbtn').onclick = function () {
+    location.reload();
+
+}
